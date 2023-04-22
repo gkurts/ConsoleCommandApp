4159e70b2d2073818a135d96616a45446cb908b1
Greg Kurts <gkurts@gregkurts.com>
2023-04-22 13:08:04 -0500

    separate out exceptions and interfaces

---
+++ ConsoleCommandApp/Exceptions/GetTemperatureException.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleCommandApp.Exceptions
+{
+    [Serializable]
+    public class GetTemperatureException : Exception
+    {
+        public GetTemperatureException()
+        { }
+
+        public GetTemperatureException(string message)
+            : base(message)
+        { }
+
+        public GetTemperatureException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}
---
+++ ConsoleCommandApp/Interfaces/IWeatherService.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleCommandApp.Interfaces
+{
+    /// <summary>
+    /// Fake weather service
+    /// </summary>
+    public interface IWeatherService
+    {
+        /// <summary>
+        /// Returns a random temperature.
+        /// </summary>
+        /// <param name="zip">Any 5 numbers will do.</param>
+        /// <returns></returns>
+        Task<int> GetTemperatureAsync(int zip);
+    }
+}
